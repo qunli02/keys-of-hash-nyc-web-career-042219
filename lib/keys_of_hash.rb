1dcc33c469677d6761abb376e5cbca115d82c0b1
Qun Li Huang <qunli02@gmail.com>
2019-04-15 21:01:23 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
   def keys_of(arguments)
     # code goes here
+    name = []
+    key = self.keys
+    value = self.values
+    until value.index(arguments) == nil
+      name << key[value.index(arguments)]
+      print name
+      key.delete_at(value.index(arguments))
+      name.delete_at(value.index(arguments))
+    end
+    return name
   end
 end
